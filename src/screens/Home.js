cc2a7e77125c8588d330ffb19e4a828616c11c18
sabirkhaloufi <sabirkhaloufi@gmail.com>
2023-01-25 21:32:44 +0100

    add background image

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,12 +1,32 @@
 import React from 'react'
-import {View, Text, ImageBackground} from 'react-native';
+import {View, Text, ImageBackground, StyleSheet, SafeAreaView, TextInput} from 'react-native';
+import background from '../../assets/images/1540030.jpg'
 
 function Home() {
   return (
-    <ImageBackground>
-        <Text>Home Page</Text>
+    <ImageBackground source={background} style={styles.image}>
+      <SafeAreaView>
+        <View style={styles.textInputContainer}>
+          <TextInput style={styles.textInput}
+          placeholder={"enter xx City"}/>
+        </View>
+      </SafeAreaView>
     </ImageBackground>
   )
 }
 
+
 export default Home
+
+
+const styles = StyleSheet.create({
+  image: {
+    flex:1,
+    // justifyContent:"center",
+    resizeMode: 'cover',
+  },
+  textInputContainer:{
+    backgroundColor: "red",
+
+  }
+})
