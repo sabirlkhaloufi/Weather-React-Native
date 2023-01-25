7ce7b7048023b0f3d78ae0c26f8f9081fa374315
sabirkhaloufi <sabirkhaloufi@gmail.com>
2023-01-25 17:49:18 +0100

    initial commit

---
+++ src/screens/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {View, Text, ImageBackground} from 'react-native';
+
+function Home() {
+  return (
+    <ImageBackground>
+        <Text>Home Page</Text>
+    </ImageBackground>
+  )
+}
+
+export default Home
