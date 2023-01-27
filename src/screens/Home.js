19805884dd7ebb202cc7156e28aad55afc8d949a
sabirkhaloufi <sabirkhaloufi@gmail.com>
2023-01-27 14:34:03 +0100

    view information from the city

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
 import {View, Text, ImageBackground, StyleSheet, SafeAreaView, TextInput} from 'react-native';
-import background from '../../assets/images/3482169.jpg'
+import background from '../../assets/images/1540030.jpg'
 import { AntDesign } from '@expo/vector-icons';
 import axios from 'axios';
 
@@ -8,7 +8,7 @@ function Home() {
 
   const [City, setCity] = useState("")
   const [weather, setWeather] = useState({})
-  const getWeather = ()=>{
+  const getWeather = ()=>{ 
     axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${City}&appid=3d5ab8472b7d63620803d4235cfe14a2`).then((response)=>{
         console.log(response.data)
         setWeather(response.data)
@@ -37,6 +37,22 @@ function Home() {
           />
 
         </View>
+
+        <View style={styles.locationContainer}>
+            <Text style={styles.location}>
+                {weather?.name} , {weather?.sys?.country}
+            </Text>
+        </View>
+
+        <View style={styles.weatherContainer}>
+            <Text style={styles.temp}>
+                {(weather?.main?.temp - 273.15).toFixed(1)} °C
+            </Text>
+
+            <Text style={styles.weather}>
+                dhdh
+            </Text>
+        </View>
         
       </SafeAreaView>
     </ImageBackground>
@@ -50,7 +66,6 @@ export default Home
 const styles = StyleSheet.create({
   image: {
     flex:1,
-    // justifyContent:"center",
     resizeMode: 'cover',
   },
   textInputContainer:{
@@ -70,5 +85,37 @@ const styles = StyleSheet.create({
     fontWeight:"600",
     fontSize: 18,
     color: "black",
+  },
+  locationContainer:{
+    marginVertical:15,
+
+  },
+  location: {
+    marginTop:5,
+    color: "#FFFFFF",
+    fontSize:35,
+    fontWeight:"900",
+    textAlign:"center",
+    textShadowColor:"rgba(0,0,0,0.55)",
+    textShadowOffset:{width:-1, height:1},
+    textShadowRadius:5,
+  },
+  weatherContainer:{
+    alignItems: "center",
+  },
+  temp:{
+    textAlign:"center",
+    color:"#FFF",
+    fontSize:100,
+    fontWeight:"800",
+    backgroundColor:"rgba(255,255,255,0.2)",
+    paddingVertical:20,
+    paddingHorizontal:30,
+    borderRadius:30,
+    overflow:"hidden",
+    marginTop:10,
+    textShadowColor:"rgba(0, 0, 0, 0.75)",
+    textShadowOffset:{width:-3, height:3},
+    textShadowRadius:10,
   }
 }) 
